b298472ecf1bd0d094d84e3ed895de905a76c2ff
Daniel Gamage <hello@danielgamage.com>
2017-03-13 06:39:08 -0400

    setting up curve

--- src/main.js
+++ src/main.js
@@ -1,34 +1,81 @@
 import {inc, dec} from './actions'
 import * as d3 from "d3"
+import {keys} from './utils'
 
 const body = document.body
 
-// const row = body.append("div")
-//   .attr("class", "row")
-// .append("div")
-//   .attr("class", "container")
-//
-// row.data(el)
-//
-// const pieChart = row.append("svg")
-//     .attr("class", `pie ${el.name}`)
-//     .attr("width",  128)
-//     .attr("height", 128)
-//     .attr("viewBox", `0 0 128 128`)
-// const arcs =  d3.pie()([...el.interpolations].map(interpolation => interpolation.weight))
-// arcs.map((arc, arcIndex) => {
-//   const arcRendered = d3.arc()
-//     .innerRadius(48)
-//     .outerRadius(64)
-//     .cornerRadius(2)
-//     .padAngle(.03)
-//     .startAngle(arc.startAngle)
-//     .endAngle(arc.endAngle)
-//
-//   pieChart.append("path")
-//       .attr("d", arcRendered)
-//       .attr("class", "arc")
-//       .attr("transform", "translate(64, 64)")
-//       .attr("fill", getColor(i, arcs.length, 90 - arcIndex * 40 / arcs.length))
-//
-// })
+var width = 400,
+    height = 430,
+    num_axes = 12,
+    tick_axis = 1,
+    start = 0,
+    end = 4;
+
+var theta = function(r) {
+  return -2*Math.PI*r;
+};
+
+var arc = d3.arc()
+  .startAngle(0)
+  .endAngle(2*Math.PI);
+
+// offset from center so there's less curl in the center
+var radius = d3.scaleLinear()
+  .domain([start, end])
+  .range([30, d3.min([width,height])/2-20]);
+
+var angle = d3.scaleLinear()
+  .domain([0,num_axes])
+  .range([0,360])
+
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width/2 + "," + (height/2+8) +")");
+
+var pieces = d3.range(start, end+0.001, (end-start)/(end*12));
+
+var spiral = d3.radialLine()
+  .angle(theta)
+  .radius(radius);
+
+svg.selectAll("circle.tick")
+    .data(d3.range(end,start,(start-end)/4))
+  .enter().append("circle")
+    .attr("class", "tick")
+    .attr("cx", 0)
+    .attr("cy", 0)
+    .attr("r", function(d) { return radius(d); })
+
+svg.selectAll(".axis")
+    .data(d3.range(num_axes))
+  .enter().append("g")
+    .attr("class", "axis")
+    .attr("transform", function(d) { return "rotate(" + -angle(d) + ")"; })
+  .call(radial_tick)
+  .append("text")
+    .attr("y", radius(end)+13)
+    .text(function(d) { return angle(d) + "°"; })
+    .attr("text-anchor", "middle")
+    .attr("transform", function(d) { return "rotate(" + -90 + ")" })
+
+svg.selectAll(".spiral")
+    .data([pieces])
+  .enter().append("path")
+    .attr("class", "spiral")
+    .attr("d", spiral)
+    .attr("transform", function(d) { return "rotate(" + 90 + ")" });
+
+function radial_tick(selection) {
+  selection.each(function(axis_num) {
+    d3.axisLeft(radius)
+      .ticks(5)
+      .tickValues( axis_num == tick_axis ? null : [])
+
+    d3.select(this)
+      .selectAll("text")
+      .attr("text-anchor", "bottom")
+      .attr("transform", "rotate(" + angle(axis_num) + ")")
+  });
+}
--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,51 @@
-export const freqForIndex = (index, array) => {
+export const freqForIndex = (index, length=49) => {
   return (2 ** ((index - 49) / 12)) * 440
 }
 
-const keyNames = [
-  "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"
+const singleOctave = [
+  { keyName: "C",  black: false, qwerty: "a" },
+  { keyName: "C#", black: true,  qwerty: "w" },
+  { keyName: "D",  black: false, qwerty: "s" },
+  { keyName: "D#", black: true,  qwerty: "e" },
+  { keyName: "E",  black: false, qwerty: "d" },
+  { keyName: "F",  black: false, qwerty: "f" },
+  { keyName: "F#", black: true,  qwerty: "t" },
+  { keyName: "G",  black: false, qwerty: "g" },
+  { keyName: "G#", black: true,  qwerty: "y" },
+  { keyName: "A",  black: false, qwerty: "h" },
+  { keyName: "A#", black: true,  qwerty: "u" },
+  { keyName: "B",  black: false, qwerty: "j" }
 ]
 
-export const keyForIndex = (index) => {
+export const noteForIndex = (index) => {
   // A0 is index=0
   // +8 passes tests but seems like it should be +9 lmao
   const adjustedIndex = index + 8
-  const key = keyNames[adjustedIndex % 12]
-  const oct = Math.floor(adjustedIndex / 12)
-  return `${key}${oct}`
+  const note = singleOctave[adjustedIndex % 12]
+  return note
+}
+
+export const keyForIndex = (index) => {
+  const adjustedIndex = index + 8
+  const note = noteForIndex(index)
+  const octave = Math.floor(adjustedIndex / 12)
+  return `${note.keyName}${octave}`
+}
+
+export const blackForIndex = (index) => {
+  return noteForIndex(index).black
+}
+
+export const qwertyForIndex = (index) => {
+  return noteForIndex(index).qwerty
 }
 
 export const keys = [...Array(49).keys()].map((el, i, arr) => {
   return {
+    index: i,
+    black: blackForIndex(el),
+    qwerty: qwertyForIndex(el),
     key: keyForIndex(el),
-    freq: freqForIndex(el, arr)
+    freq: freqForIndex(el)
   }
 })
