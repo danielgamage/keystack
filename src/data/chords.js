418008cd5ab4e06f503cfc525323c604bcae5fcb
Daniel Gamage <hello@danielgamage.com>
2017-03-14 15:35:26 -0400

    adding chord data from wiki

---
+++ src/data/chords.js
@@ -0,0 +1,772 @@
+export default [
+  {
+    "name": "Augmented chord",
+    "set": [
+      0,
+      4,
+      8
+    ],
+    "quality": "Augmented"
+  },
+  {
+    "name": "Augmented eleventh chord[1]",
+    "set": [
+      0,
+      4,
+      7,
+      10,
+      2,
+      6
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Augmented major seventh chord",
+    "set": [
+      0,
+      4,
+      8,
+      11
+    ],
+    "quality": "Augmented"
+  },
+  {
+    "name": "Augmented seventh chord",
+    "set": [
+      0,
+      4,
+      8,
+      10
+    ],
+    "quality": "Augmented"
+  },
+  {
+    "name": "Augmented sixth chord",
+    "set": [
+      0,
+      6,
+      8
+    ],
+    "quality": "Predominant"
+  },
+  {
+    "name": "Diminished chord",
+    "set": [
+      0,
+      3,
+      6
+    ],
+    "quality": "Diminished"
+  },
+  {
+    "name": "Diminished major seventh chord",
+    "set": [
+      0,
+      3,
+      6,
+      11
+    ],
+    "quality": "Diminished"
+  },
+  {
+    "name": "Diminished seventh chord",
+    "set": [
+      0,
+      3,
+      6,
+      9
+    ],
+    "quality": "Diminished"
+  },
+  {
+    "name": "Dominant",
+    "set": [
+      0,
+      4,
+      7
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Dominant eleventh chord",
+    "set": [
+      0,
+      4,
+      7,
+      10,
+      2,
+      5
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Dominant minor ninth",
+    "set": [
+      0,
+      4,
+      7,
+      10,
+      1
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Dominant ninth",
+    "set": [
+      0,
+      4,
+      7,
+      10,
+      2
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Dominant parallel",
+    "set": [
+      0,
+      3,
+      7
+    ],
+    "quality": "Minor"
+  },
+  {
+    "name": "Dominant seventh chord",
+    "set": [
+      0,
+      4,
+      7,
+      10
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Dominant seventh flat five chord",
+    "set": [
+      0,
+      4,
+      6,
+      10
+    ],
+    "quality": "Diminished"
+  },
+  {
+    "name": "Dominant seventh raised ninth/Dominant seventh split third chord",
+    "set": [
+      0,
+      4,
+      7,
+      10,
+      3
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Dominant thirteenth chord",
+    "set": [
+      0,
+      4,
+      7,
+      10,
+      2,
+      5,
+      9
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Dream chord",
+    "set": [
+      0,
+      5,
+      6,
+      7
+    ],
+    "quality": "Just"
+  },
+  {
+    "name": "Elektra chord",
+    "set": [
+      0,
+      7,
+      9,
+      1,
+      4
+    ],
+    "quality": "Bitonal"
+  },
+  {
+    "name": "Farben chord",
+    "set": [
+      0,
+      8,
+      11,
+      4,
+      9
+    ],
+    "quality": "Atonal"
+  },
+  {
+    "name": "Half-diminished seventh chord",
+    "set": [
+      0,
+      3,
+      6,
+      10
+    ],
+    "quality": "Diminished"
+  },
+  {
+    "name": "Harmonic seventh chord",
+    "set": [
+      0,
+      4,
+      7,
+      10
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Hendrix chord/Augmented ninth[9]",
+    "set": [
+      0,
+      4,
+      7,
+      10,
+      3
+    ],
+    "quality": "Mixed"
+  },
+  {
+    "name": "Leading-tone chord",
+    "set": [
+      0,
+      3,
+      6
+    ],
+    "quality": "Diminished"
+  },
+  {
+    "name": "Lydian chord",
+    "set": [
+      0,
+      4,
+      7,
+      11,
+      6
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Magic chord",
+    "set": [
+      0,
+      1,
+      5,
+      6,
+      10,
+      0,
+      3,
+      5
+    ],
+    "quality": "Just"
+  },
+  {
+    "name": "Major chord",
+    "set": [
+      0,
+      4,
+      7
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Major eleventh chord",
+    "set": [
+      0,
+      4,
+      7,
+      11,
+      2,
+      5
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Major seventh chord",
+    "set": [
+      0,
+      4,
+      7,
+      11
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Major seventh sharp eleventh chord",
+    "set": [
+      0,
+      4,
+      8,
+      11,
+      6
+    ],
+    "quality": "Augmented"
+  },
+  {
+    "name": "Major sixth chord",
+    "set": [
+      0,
+      4,
+      7,
+      9
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Major sixth ninth chord (\"6 add 9\",[11] Nine six,[12] 6/9)",
+    "set": [
+      0,
+      4,
+      7,
+      9,
+      2
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Major ninth chord",
+    "set": [
+      0,
+      4,
+      7,
+      11,
+      2
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Major thirteenth chord",
+    "set": [
+      0,
+      4,
+      7,
+      11,
+      2,
+      6,
+      9
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Mediant",
+    "set": [
+      0,
+      3,
+      7
+    ],
+    "quality": "Minor"
+  },
+  {
+    "name": "Minor chord",
+    "set": [
+      0,
+      3,
+      7
+    ],
+    "quality": "Minor"
+  },
+  {
+    "name": "Minor eleventh chord",
+    "set": [
+      0,
+      3,
+      7,
+      10,
+      2,
+      5
+    ],
+    "quality": "Minor"
+  },
+  {
+    "name": "Minor major seventh chord",
+    "set": [
+      0,
+      3,
+      7,
+      11
+    ],
+    "quality": "Minor"
+  },
+  {
+    "name": "Minor ninth chord",
+    "set": [
+      0,
+      3,
+      7,
+      10,
+      2
+    ],
+    "quality": "Minor"
+  },
+  {
+    "name": "Minor seventh chord",
+    "set": [
+      0,
+      3,
+      7,
+      10
+    ],
+    "quality": "Minor"
+  },
+  {
+    "name": "Minor sixth chord",
+    "set": [
+      0,
+      3,
+      7,
+      9
+    ],
+    "quality": "Minor"
+  },
+  {
+    "name": "Minor sixth ninth chord (6/9)",
+    "set": [
+      0,
+      3,
+      7,
+      9,
+      2
+    ],
+    "quality": "Minor"
+  },
+  {
+    "name": "Minor thirteenth chord",
+    "set": [
+      0,
+      3,
+      7,
+      10,
+      2,
+      5,
+      9
+    ],
+    "quality": "Minor"
+  },
+  {
+    "name": "Mu chord",
+    "set": [
+      0,
+      2,
+      4,
+      7
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Mystic chord",
+    "set": [
+      0,
+      6,
+      10,
+      4,
+      9,
+      2
+    ],
+    "quality": "Atonal"
+  },
+  {
+    "name": "Neapolitan chord",
+    "set": [
+      1,
+      5,
+      8
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Ninth augmented fifth chord[11][13]",
+    "set": [
+      0,
+      4,
+      8,
+      10,
+      2
+    ],
+    "quality": "Augmented"
+  },
+  {
+    "name": "Ninth flat fifth chord[11]",
+    "set": [
+      0,
+      4,
+      6,
+      10,
+      2
+    ],
+    "quality": "M3+d5"
+  },
+  {
+    "name": "Northern lights chord",
+    "set": [
+      1,
+      2,
+      8,
+      0,
+      3,
+      6,
+      7,
+      10,
+      11,
+      4,
+      7
+    ],
+    "quality": "Atonal"
+  },
+  {
+    "name": "\"Ode-to-Napoleon\" hexachord",
+    "set": [
+      0,
+      1,
+      4,
+      5,
+      8,
+      9
+    ],
+    "quality": "Atonal"
+  },
+  {
+    "name": "Petrushka chord",
+    "set": [
+      0,
+      1,
+      4,
+      6,
+      7,
+      10
+    ],
+    "quality": "Mixed"
+  },
+  {
+    "name": "Power chord",
+    "set": [
+      0,
+      7
+    ],
+    "quality": "Indeterminate"
+  },
+  {
+    "name": "Psalms chord",
+    "set": [
+      0,
+      3,
+      7
+    ],
+    "quality": "Minor"
+  },
+  {
+    "name": "Secondary dominant",
+    "set": [
+      0,
+      4,
+      7
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Secondary leading-tone chord",
+    "set": [
+      0,
+      3,
+      6
+    ],
+    "quality": "Diminished"
+  },
+  {
+    "name": "Secondary supertonic chord",
+    "set": [
+      0,
+      3,
+      7
+    ],
+    "quality": "Minor"
+  },
+  {
+    "name": "Seven six chord[16]",
+    "set": [
+      0,
+      4,
+      7,
+      9,
+      10
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Seventh flat nine chord[18]",
+    "set": [
+      0,
+      4,
+      7,
+      10,
+      1
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Seventh suspension four chord[19]",
+    "set": [
+      0,
+      5,
+      7,
+      10
+    ],
+    "quality": "Suspended"
+  },
+  {
+    "name": "So What chord",
+    "set": [
+      0,
+      5,
+      10,
+      3,
+      7
+    ],
+    "quality": "Bitonal"
+  },
+  {
+    "name": "Suspended chord",
+    "set": [
+      0,
+      5,
+      7
+    ],
+    "quality": "Indeterminate"
+  },
+  {
+    "name": "Subdominant",
+    "set": [
+      0,
+      4,
+      7
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Subdominant parallel",
+    "set": [
+      0,
+      3,
+      7
+    ],
+    "quality": "Minor"
+  },
+  {
+    "name": "Submediant",
+    "set": [
+      0,
+      3,
+      7
+    ],
+    "quality": "Minor"
+  },
+  {
+    "name": "Subtonic",
+    "set": [
+      0,
+      4,
+      7
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Supertonic",
+    "set": [
+      0,
+      3,
+      7
+    ],
+    "quality": "Minor"
+  },
+  {
+    "name": "Thirteenth flat ninth chord[11]",
+    "set": [
+      0,
+      4,
+      7,
+      10,
+      1,
+      5,
+      9
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Thirteenth flat ninth flat fifth chord[11]",
+    "set": [
+      0,
+      4,
+      6,
+      10,
+      1,
+      5,
+      9
+    ],
+    "quality": "M3+d5"
+  },
+  {
+    "name": "Tonic counter parallel",
+    "set": [
+      0,
+      3,
+      7
+    ],
+    "quality": "Minor"
+  },
+  {
+    "name": "Tonic",
+    "set": [
+      0,
+      4,
+      7
+    ],
+    "quality": "Major"
+  },
+  {
+    "name": "Tonic parallel",
+    "set": [
+      0,
+      3,
+      7
+    ],
+    "quality": "Minor"
+  },
+  {
+    "name": "Tristan chord",
+    "set": [
+      0,
+      3,
+      6,
+      10
+    ],
+    "quality": "Predominant"
+  },
+  {
+    "name": "Viennese trichord",
+    "set": [
+      0,
+      1,
+      6
+    ],
+    "quality": "Atonal"
+  }
+  {
+    "name": "Viennese trichord (alt)",
+    "set": [
+      0,
+      6,
+      7
+    ],
+    "quality": "Atonal"
+  }
+]
