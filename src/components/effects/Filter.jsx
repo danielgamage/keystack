7c069b41799c2d385500b9161b4b6e14f3919566
Daniel Gamage <hello@danielgamage.com>
2017-03-23 17:13:10 -0400

    fix for d3 funcs with multiple filters

--- src/components/effects/Filter.jsx
+++ src/components/effects/Filter.jsx
@@ -3,42 +3,42 @@ import { connect } from 'preact-redux'
 
 import { audioEffectNodes } from '../../utils/audio'
 
-import { line, curveCatmullRom } from "d3-shape"
-import { scaleLinear, scaleLog, scalePow } from "d3-scale"
-import { axisBottom, axisLeft } from "d3-axis"
-import { select } from "d3-selection"
+import { line, curveCatmullRom } from 'd3-shape'
+import { scaleLinear, scaleLog, scalePow } from 'd3-scale'
+import { axisBottom, axisLeft } from 'd3-axis'
+import { select } from 'd3-selection'
 
 import Item from '../Item.jsx'
 import NumericInput from '../NumericInput.jsx'
 
 const filterTypes = [
-  { name: "lowpass",   frequency: true, q: true,  gain: false },
-  { name: "highpass",  frequency: true, q: true,  gain: false },
-  { name: "bandpass",  frequency: true, q: true,  gain: false },
-  { name: "lowshelf",  frequency: true, q: false, gain: true  },
-  { name: "highshelf", frequency: true, q: false, gain: true  },
-  { name: "peaking",   frequency: true, q: true,  gain: true  },
-  { name: "notch",     frequency: true, q: true,  gain: false },
-  { name: "allpass",   frequency: true, q: true,  gain: false },
+  { name: 'lowpass',   frequency: true, q: true,  gain: false },
+  { name: 'highpass',  frequency: true, q: true,  gain: false },
+  { name: 'bandpass',  frequency: true, q: true,  gain: false },
+  { name: 'lowshelf',  frequency: true, q: false, gain: true  },
+  { name: 'highshelf', frequency: true, q: false, gain: true  },
+  { name: 'peaking',   frequency: true, q: true,  gain: true  },
+  { name: 'notch',     frequency: true, q: true,  gain: false },
+  { name: 'allpass',   frequency: true, q: true,  gain: false },
 ]
 
 const parameters = [
-  { name: "frequency",
-    unit: "hz",
-    format: ".3s",
+  { name: 'frequency',
+    unit: 'hz',
+    format: '.3s',
     min: 30,
     max: 22000,
     step: 0.01,
     scale: 10
   },
-  { name: "q",
+  { name: 'q',
     min: 0.1,
     max: 18,
     step: 0.1,
     scale: Math.E
   },
-  { name: "gain",
-    unit: "dB",
+  { name: 'gain',
+    unit: 'dB',
     min: -15,
     max: 15,
     step: 0.1,
@@ -46,72 +46,68 @@ const parameters = [
   }
 ]
 
-const viewBoxWidth = 256
-const viewBoxHeight = 64
-const frequencyBars = 1024
-const minHz = 30
-const maxHz = 22000
-
-const mapFreq = scalePow()
-  .exponent(5)
-  .domain([
-    0,
-    frequencyBars
-  ])
-  .range([
-    minHz,
-    maxHz
-  ])
-
-const x = scaleLog()
-  .domain([minHz, maxHz])
-  .range([0, viewBoxWidth])
-const y = scaleLinear()
-  .domain([-1, 3])
-  .range([viewBoxHeight, 0])
-  .clamp(true)
-const envelopePath = line()
-  .x((d) => x(d.x) )
-  .y((d) => y(d.y) )
-  .curve(curveCatmullRom)
-
-const xAxis = axisBottom()
-  .scale(x)
-  .ticks(3, ".1s")
-const yAxis = axisLeft()
-  .scale(y)
-  .ticks(5, ".1s")
-
 class Filter extends Component {
-  componentDidMount() {
-    const axis = select(`#vis-${this.props.data.id} .axis-x`)
-      .call(xAxis.tickSize('5'))
-      .selectAll('*')
-        .attr('vector-effect', "non-scaling-stroke")
-    const gridX = select(`#vis-${this.props.data.id} .grid-x`)
-      .call(xAxis
-        .tickSize(viewBoxHeight)
-        .tickFormat("")
-      )
-      .selectAll('*')
-        .attr('vector-effect', "non-scaling-stroke");
-    const gridY = select(`#vis-${this.props.data.id} .grid-y`)
-      .call(yAxis
-        .tickSize(viewBoxWidth)
-        .tickFormat("")
-      )
-      .attr('transform', `translate(${viewBoxWidth},0)`)
-      .selectAll('*')
-        .attr('vector-effect', "non-scaling-stroke");
+  constructor (props) {
+    super(props)
+    this.viewBoxWidth = 256
+    this.viewBoxHeight = 64
+    this.frequencyBars = 1024
+    this.minHz = 30
+    this.maxHz = 22000
+
+    this.mapFreq = scalePow()
+      .exponent(5)
+      .domain([0, this.frequencyBars])
+      .range([this.minHz, this.maxHz])
+
+    this.x = scaleLog()
+      .domain([this.minHz, this.maxHz])
+      .range([0, this.viewBoxWidth])
+    this.y = scaleLinear()
+      .domain([-1, 3])
+      .range([this.viewBoxHeight, 0])
+      .clamp(true)
+    this.envelopePath = line()
+      .x((d) => this.x(d.x))
+      .y((d) => this.y(d.y))
+      .curve(curveCatmullRom)
+
+    this.xAxis = axisBottom()
+      .scale(this.x)
+      .ticks(3, '.1s')
+    this.yAxis = axisLeft()
+      .scale(this.y)
+      .ticks(5, '.1s')
   }
-	render() {
-    var myFrequencyArray = new Float32Array(frequencyBars);
-    for(var i = 0; i < frequencyBars; ++i) {
-      myFrequencyArray[i] = mapFreq(i)
+  componentDidMount () {
+    const axis = select(`#vis-${this.props.data.id} .axis-x`)
+      .call(this.xAxis.tickSize('5'))
+      .selectAll('*')
+        .attr('vector-effect', 'non-scaling-stroke')
+    const gridX = select(`#vis-${this.props.data.id} .grid-x`)
+      .call(this.xAxis
+        .tickSize(this.viewBoxHeight)
+        .tickFormat('')
+      )
+    .selectAll('*')
+    .attr('vector-effect', 'non-scaling-stroke')
+    const gridY = select(`#vis-${this.props.data.id} .grid-y`)
+      .call(this.yAxis
+        .tickSize(this.viewBoxWidth)
+        .tickFormat('')
+      )
+      .attr('transform', `translate(${this.viewBoxWidth}, 0)`)
+    .selectAll('*')
+      .attr('vector-effect', 'non-scaling-stroke')
+  }
+  render () {
+    var myFrequencyArray = new Float32Array(this.frequencyBars)
+    for (var i = 0; i < this.frequencyBars; ++i) {
+      myFrequencyArray[i] = this.mapFreq(i)
     }
 
-    var magResponseOutput = new Float32Array(frequencyBars) // magnitude
-    var phaseResponseOutput = new Float32Array(frequencyBars)
+    var magResponseOutput = new Float32Array(this.frequencyBars) // magnitude
+    var phaseResponseOutput = new Float32Array(this.frequencyBars)
     const filterNode = audioEffectNodes.find(el => el.id === this.props.data.id).node
     filterNode.getFrequencyResponse(
       myFrequencyArray,
@@ -121,47 +117,47 @@ class Filter extends Component {
     const magnitudePoints = [...magResponseOutput].map((response, i) => ({x: myFrequencyArray[i], y: response}))
     const phasePoints = [...phaseResponseOutput].map((response, i) => ({x: myFrequencyArray[i], y: response}))
 
-		return (
-      <Item title="Filter" type="audio" item={this.props.data}
-        headerChildren={<div className="select">
-                    <select
-                      onChange={(e) => {
-                        this.props.dispatch({
-                          type: 'UPDATE_EFFECT',
-                          id: this.props.data.id,
-                          property: 'type',
-                          value: e.target.value
-                        })
-                      }}
-                      >
-                      {filterTypes.map(type => (
-                        <option value={type.name}>{type.name}</option>
-                      ))}
-                    </select>
-                  </div>}>
-        <svg class="vis-path" id={`vis-${this.props.data.id}`} viewBox={`0 0 ${viewBoxWidth} ${viewBoxHeight}`}>
-          <g class="grid grid-x" />
-          <g class="grid grid-y" />
+    return (
+      <Item title='Filter' type='audio' item={this.props.data}
+        headerChildren={<div className='select'>
+          <select
+            onChange={(e) => {
+              this.props.dispatch({
+                type: 'UPDATE_EFFECT',
+                id: this.props.data.id,
+                property: 'type',
+                value: e.target.value
+              })
+            }}
+            >
+            {filterTypes.map(type => (
+              <option value={type.name}>{type.name}</option>
+            ))}
+          </select>
+        </div>}>
+        <svg class='vis-path' id={`vis-${this.props.data.id}`} viewBox={`0 0 ${this.viewBoxWidth} ${this.viewBoxHeight}`}>
+          <g class='grid grid-x' />
+          <g class='grid grid-y' />
           <path
-            class="filter-phase"
-            vector-effect="non-scaling-stroke"
-            d={envelopePath(phasePoints)}
+            class='filter-phase'
+            vector-effect='non-scaling-stroke'
+            d={this.envelopePath(phasePoints)}
             />
           <path
-            class="filter-magnitude"
-            vector-effect="non-scaling-stroke"
-            d={envelopePath(magnitudePoints)}
+            class='filter-magnitude'
+            vector-effect='non-scaling-stroke'
+            d={this.envelopePath(magnitudePoints)}
             />
           <g
-            class="graph-axis axis-x"
-            transform={`translate(0,${viewBoxHeight})`}
+            class='graph-axis axis-x'
+            transform={`translate(0,${this.viewBoxHeight})`}
             />
         </svg>
-        <div class="flex-container">
+        <div class='flex-container'>
           {parameters.map(param => (
             <NumericInput
               label={param.name}
-              class="tri"
+              class='tri'
               disabled={!filterTypes.find(el => el.name === this.props.data.type)[param.name]}
               id={param.name}
               min={param.min}
@@ -180,8 +176,8 @@ class Filter extends Component {
           ))}
         </div>
       </Item>
-		)
-	}
+    )
+  }
 }
 
 export default connect()(Filter)
