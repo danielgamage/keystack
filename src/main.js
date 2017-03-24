a0917c82b6f3538f24add13a7a879cd005ce5f55
Daniel Gamage <hello@danielgamage.com>
2017-03-24 13:44:45 -0400

    prevent notes on renaming

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
-import { select, selectAll } from "d3-selection"
-import { axisLeft } from "d3-axis"
-import { scaleLinear } from "d3-scale"
-import { radialLine } from "d3-shape"
-import { range } from "d3-array"
+import { select, selectAll } from 'd3-selection'
+import { axisLeft } from 'd3-axis'
+import { scaleLinear } from 'd3-scale'
+import { radialLine } from 'd3-shape'
+import { range } from 'd3-array'
 
-import { keys, noteForIndex } from './utils'
+import { keys } from './utils'
 import { stopNote, startNote } from './utils/notes'
 import keySteps from './data/keySteps'
-import chords from './data/chords'
 
 import { h, render } from 'preact'
 import { Provider } from 'preact-redux'
@@ -22,9 +21,9 @@ import './styles/style.scss'
 //
 
 render((
-	<Provider store={store}>
-		<App />
-	</Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 ), document.querySelector('#root'))
 
 //
@@ -33,21 +32,25 @@ render((
 
 let octave = 3
 
-window.addEventListener("keydown", (event) => {
-  if (keySteps.some(keyStep => keyStep.code === event.keyCode)) {
-    const steps = keySteps.filter(keyStep => event.keyCode === keyStep.code)[0].step
-    const note = keys[steps + 2 + (octave * 12)]
-    startNote(note)
+window.addEventListener('keydown', (event) => {
+  if (!document.activeElement.classList.contains('item-title')) {
+    if (keySteps.some(keyStep => keyStep.code === event.keyCode)) {
+      const steps = keySteps.filter(keyStep => event.keyCode === keyStep.code)[0].step
+      const note = keys[steps + 2 + (octave * 12)]
+      startNote(note)
+    }
   }
 })
-window.addEventListener("keyup", (event) => {
-  if (event.keyCode === 90) { // z
-    octave = Math.max(--octave, 0)
-  } else if (event.keyCode === 88) { // x
-    octave = Math.min(++octave, 5)
-  } else if (keySteps.some(keyStep => keyStep.code === event.keyCode)) {
-    const steps = keySteps.filter(keyStep => event.keyCode === keyStep.code)[0].step
-    const note = keys[steps + 2 + (octave * 12)]
-    stopNote(note)
+window.addEventListener('keyup', (event) => {
+  if (!document.activeElement.classList.contains('item-title')) {
+    if (event.keyCode === 90) { // z
+      octave = Math.max(--octave, 0)
+    } else if (event.keyCode === 88) { // x
+      octave = Math.min(++octave, 5)
+    } else if (keySteps.some(keyStep => keyStep.code === event.keyCode)) {
+      const steps = keySteps.filter(keyStep => event.keyCode === keyStep.code)[0].step
+      const note = keys[steps + 2 + (octave * 12)]
+      stopNote(note)
+    }
   }
 })
