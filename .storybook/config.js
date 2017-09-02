f1d59b3fb187b944e2c9f08aff8a01e7a1f65ac5
Daniel Gamage <hello@danielgamage.com>
2017-09-02 12:05:42 -0400

    throttled knob calls

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,5 @@
 import { configure, addDecorator } from '@storybook/react'
-import { withKnobs } from '@storybook/addon-knobs'
+import { withKnobsOptions } from '@storybook/addon-knobs'
 import '../src/styles/global.sass'
 
 const req = require.context('../.stories', true, /\.js$/)
@@ -8,6 +8,9 @@ function loadStories () {
   req.keys().forEach((filename) => req(filename))
 }
 
-addDecorator(withKnobs)
+addDecorator(withKnobsOptions({
+  debounce: { wait: 5000, leading: true }, // Same as lodash debounce.
+  timestamps: true // Doesn't emit events while user is typing.
+}))
 
 configure(loadStories, module)
