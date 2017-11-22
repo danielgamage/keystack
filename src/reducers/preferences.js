29c492644c2526ee85db33f8e7fa338e87d4f493
Daniel Gamage <hello@danielgamage.com>
2017-11-22 04:40:18 -0500

    fixed up theme reducer

--- src/reducers/preferences.js
+++ src/reducers/preferences.js
@@ -1,12 +1,17 @@
 const defaultState = {
-  draggingTrackItem: false
+  theme: {
+    lightness: 'light',
+    accent: 'green',
+  },
 }
+
 const view = (state = defaultState, action) => {
   switch (action.type) {
-    case 'UPDATE_VIEW':
+    case 'UPDATE_THEME':
       let newState = {...state}
-      newState[action.property] = action.value
+      newState.theme = action.value
       return newState
+
     default:
       return state
   }
