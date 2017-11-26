f11929fb955fad026d1015b345ea1434a5663e6e
Daniel Gamage <hello@danielgamage.com>
2017-11-26 04:28:00 -0500

    added theme to all stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,7 @@
 import { configure, addDecorator } from '@storybook/react'
 import { withKnobsOptions } from '@storybook/addon-knobs'
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
 
 const req = require.context('../.stories', true, /\.js$/)
 
@@ -11,5 +13,18 @@ addDecorator(withKnobsOptions({
   // debounce: { wait: 200, leading: true }, // Same as lodash debounce.
   timestamps: true // Doesn't emit events while user is typing.
 }))
+
+const theme = {
+  accent: 'orange',
+  lightness: 'light',
+}
+
+addDecorator(function(getStory) {
+  return (
+    <ThemeProvider theme={theme}>
+      {getStory()}
+    </ThemeProvider>
+  )
+})
 
 configure(loadStories, module)
