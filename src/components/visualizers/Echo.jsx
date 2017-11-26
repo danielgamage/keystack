68a658cdf615865583c0a5b8cdb8beba3a0cb3ec
Daniel Gamage <hello@danielgamage.com>
2017-11-26 05:01:15 -0500

    optimized echo,jsx

--- src/components/visualizers/Echo.jsx
+++ src/components/visualizers/Echo.jsx
@@ -71,29 +71,39 @@ class Echo extends Component {
     this.mouseDown = this.mouseDown.bind(this)
     this.mouseUp = this.mouseUp.bind(this)
     this.drag = this.drag.bind(this)
+
+    this.setScales()
+  }
+
+  componentDidMount () {
+    this.renderAxes()
   }
 
   updateValue (index, value) {
     let newValue = [...this.props.value]
     newValue[index] = value
 
-    this.props.onInput(newValue)
+    // this.props.onInput(newValue)
   }
 
-  mouseDown (e) {
-    console.log('mousedown')
+  //
+  //
+  //
 
+  mouseDown (e) {
     window.addEventListener('mousemove', this.drag)
     window.addEventListener('mouseup', this.mouseUp)
     this.drag(e)
   }
 
   mouseUp () {
-    console.log('mouseup')
-
     window.removeEventListener('mousemove', this.drag)
     window.removeEventListener('mouseup', this.mouseUp)
   }
+
+  //
+  //
+  //
 
   drag (e) {
     const containerBox = this.containerElement.getBoundingClientRect()
@@ -135,48 +145,55 @@ class Echo extends Component {
     return delayIndex
   }
 
-  render () {
-    console.log('render echo.jsx')
-    console.log('delayTime', this.props.delayTime, 'feedback', this.props.feedback)
-    const sizeScale = scaleLinear()
+  setScales () {
+    this.sizeScale = scaleLinear()
       .domain([0, 128])
       .range([0.1, 1])
+    this.delayCount = this.getDelayCount()
+    this.xScale = scaleLinear()
+      .domain([0, (this.delayCount * this.props.delayTime)])
+      .range([0, 256 - 28])
+  }
 
-    const delayCount = this.getDelayCount()
+  //
+  //
+  //
 
-    const x = scaleLinear()
-      .domain([0, (delayCount * this.props.delayTime)])
-      .range([0, 256 - 48])
-
-    const delays = Array(delayCount + 1).fill().map((el, i) => {
-      if (i === 0) return {
-        offset: 0,
-        size: sizeScale(128 * (1 - this.props.mix))
-      }
-
-      else return {
-        offset: x(i * this.props.delayTime),
-        size: sizeScale(128 * this.props.mix * (Math.pow(this.props.feedback, (i - 1))))
-      }
-    }).reverse()
-
-    const xAxisScale = scaleLinear()
-      .domain([0, (delayCount * this.props.delayTime)])
-      .range([64, 256 - 48])
-
+  renderAxes () {
     const formatSeconds = format('.1r')
 
     const xAxis = axisBottom()
-      .scale(x)
+      .scale(this.xScale)
       .ticks(5)
       .tickFormat(d => formatSeconds(d) + 's')
-      .tickSizeOuter([4])
+      .tickSizeOuter([6])
       .tickSizeInner([2])
 
-    const axis = select(`.axis-x`)
+    const axis = select(`.axis-x[data-id="${this.props.id}"]`)
       .call(xAxis.tickSize('5'))
       .selectAll('*')
         .attr('vector-effect', 'non-scaling-stroke')
+  }
+
+  //
+  //
+  //
+
+  render () {
+    this.setScales()
+    this.renderAxes()
+
+    const delays = Array(this.delayCount + 1).fill().map((el, i) => {
+      if (i === 0) return {
+        offset: 0,
+        size: this.sizeScale(128 * (1 - this.props.mix))
+      }
+
+      else return {
+        offset: this.xScale(i * this.props.delayTime),
+        size: this.sizeScale(128 * this.props.mix * (Math.pow(this.props.feedback, (i - 1))))
+      }
+    }).reverse()
 
     return (
       <EchoContainer>
@@ -187,7 +204,7 @@ class Echo extends Component {
                 <line
                   className="grid-line"
                   vectorEffect='non-scaling-stroke'
-                  x1="32" y1="8" x2="32" y2="120"
+                  x1="24" y1="0" x2="24" y2="128"
                 />
               </g>
             ))}
@@ -198,26 +215,26 @@ class Echo extends Component {
               <g transform={`translate(${el.offset} 0)`}>
                 <g>
                   <ellipse
-                    class="handle"
+                    className="handle"
                     vectorEffect='non-scaling-stroke'
-                    cx="32" cy="64" rx="1" ry="2.5"
+                    cx="24" cy="64" rx="1" ry="2.5"
                   />
                 </g>
 
                 <g
                   transform={`
-                    translate(32 64)
+                    translate(24 64)
                     scale(${el.size})
                     translate(-32 -64)
                   `}
                 >
                 	<path
-                    class={"st1 " + (i == arr.length - 1 ? 'first' : '')}
+                    className={"st1 " + (i == arr.length - 1 ? 'first' : '')}
                     vectorEffect='non-scaling-stroke'
                     d="M32,120C18.7,120,8,94.9,8,64S18.7,8,32,8"
                   />
                 	<path
-                    class={"st2 " + (i == arr.length - 1 ? 'first' : '')}
+                    className={"st2 " + (i == arr.length - 1 ? 'first' : '')}
                     vectorEffect='non-scaling-stroke'
                     d="M32,8c13.3,0,24,25.1,24,56s-10.7,56-24,56"
                   />
@@ -228,7 +245,8 @@ class Echo extends Component {
 
           <g
             className='graph-axis axis-x'
-            transform={`translate(32,116)`}
+            data-id={this.props.id}
+            transform={`translate(24,128)`}
           />
         </svg>
       </EchoContainer>
