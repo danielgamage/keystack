31664254bcbba82913d4a84bf088a28c744fd3a3
Daniel Gamage <hello@danielgamage.com>
2021-01-02 04:10:47 -0500

    knob styling bump

--- src/components/elements/Knob.jsx
+++ src/components/elements/Knob.jsx
@@ -58,7 +58,7 @@ export const StyledKnob = styled.div`
     .theme--dark & {
       box-shadow: 0 2px 10px rgba(0, 0, 0, 0.5),
         0 1px 0 -0.5px rgba(255, 255, 255, 0.2) inset,
-        0 -4px 10px -4px rgba(0, 0, 0, 0.2) inset;
+        0 -4px 10px -4px rgba(0, 0, 0, 0.3) inset;
     }
   }
   .fader-track {
